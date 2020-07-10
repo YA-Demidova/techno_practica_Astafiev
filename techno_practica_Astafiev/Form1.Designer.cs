fbf0ae30da2cf968bb6691e1b1e074720809c7c2
Anton_Demidov <aademidov@list.ru>
2020-07-10 10:36:18 +0300

    Добавьте файлы проекта.

---
+++ techno_practica_Astafiev/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace techno_practica_Astafiev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.dataGV = new System.Windows.Forms.DataGridView();
+            this.chrLineAPX = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRandom = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chrLineAPX)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGV
+            // 
+            this.dataGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGV.Location = new System.Drawing.Point(2, 3);
+            this.dataGV.Name = "dataGV";
+            this.dataGV.Size = new System.Drawing.Size(245, 275);
+            this.dataGV.TabIndex = 0;
+            // 
+            // chrLineAPX
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chrLineAPX.ChartAreas.Add(chartArea2);
+            this.chrLineAPX.Location = new System.Drawing.Point(253, 40);
+            this.chrLineAPX.Name = "chrLineAPX";
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.FastPoint;
+            series3.Name = "Series1";
+            series3.YValuesPerPoint = 2;
+            series4.ChartArea = "ChartArea1";
+            series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series4.Name = "Series2";
+            this.chrLineAPX.Series.Add(series3);
+            this.chrLineAPX.Series.Add(series4);
+            this.chrLineAPX.Size = new System.Drawing.Size(404, 238);
+            this.chrLineAPX.TabIndex = 1;
+            this.chrLineAPX.Text = "chart1";
+            // 
+            // btnStart
+            // 
+            this.btnStart.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(204)));
+            this.btnStart.Location = new System.Drawing.Point(351, 6);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(92, 31);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "График";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(469, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnRandom.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World, ((byte)(204)));
+            this.btnRandom.Location = new System.Drawing.Point(253, 6);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(92, 31);
+            this.btnRandom.TabIndex = 7;
+            this.btnRandom.Text = "Заполняем";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 285);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.chrLineAPX);
+            this.Controls.Add(this.dataGV);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chrLineAPX)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGV;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chrLineAPX;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRandom;
+    }
+}
+
---
+++ techno_practica_Astafiev/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace techno_practica_Astafiev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            x = new List<float>();
+            y = new List<float>();
+            x1 = new List<float>();
+            y1 = new List<float>();
+            dataGV.ColumnCount = 2;
+
+        }
+
+        public List<float> x;
+        public List<float> y;
+        public List<float> y1;
+        public List<float> x1;
+        float a, b, SumXY, SumX2, SumX, SumY;
+        Random rnd = new Random();
+
+        private void btnRandom_Click(object sender, EventArgs e)
+        {
+            dataGV.RowCount = 10;
+            for (int X=0;X<9;X++)
+            {
+                dataGV.Rows[X].Cells[0].Value = X;
+                dataGV.Rows[X].Cells[1].Value = X * rnd.Next(0, 10); 
+            }
+
+
+            for (int i = 0; i < dataGV.RowCount - 1; i++)
+            {
+                x.Add((float)Convert.ToDouble(dataGV.Rows[i].Cells[0].Value));
+                y.Add((float)Convert.ToDouble(dataGV.Rows[i].Cells[1].Value));
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            chrLineAPX.Series[0].Points.DataBindXY(x, y);
+            chrLineAPX.Series[1].Points.DataBindXY(x1, y1);
+
+            for (int i = 0; i < dataGV.RowCount - 1; i++)
+            {
+                x.Add((float)Convert.ToDouble(dataGV.Rows[i].Cells[0].Value));
+                y.Add((float)Convert.ToDouble(dataGV.Rows[i].Cells[1].Value));
+            }
+            APX();
+            chrLineAPX.ChartAreas[0].AxisX.Maximum = dataGV.RowCount - 1;
+            x1.Add(0); 
+            x1.Add(Convert.ToInt32(chrLineAPX.ChartAreas[0].AxisX.Maximum));
+
+            y1.Add(Convert.ToInt64(b)); 
+            y1.Add(Convert.ToInt32(a * x1[1] + b));
+
+            chrLineAPX.Series[0].Points.DataBindXY(x, y);
+            chrLineAPX.Series[1].Points.DataBindXY(x1, y1);
+
+            if (b < 0)
+                label1.Text = "y = " + a + "x - " + Math.Abs(b);
+            else
+                label1.Text = "y = " + a + "x + " + b;
+        }
+
+        private void APX()
+        {
+            SumX = 0; SumY = 0; SumXY = 0; SumX2 = 0;
+            for (int i = 0; i < x.Count; i++)
+            {
+                SumX += x[i];
+                SumY += y[i];
+                SumXY += x[i] * y[i];
+                SumX2 += x[i] * x[i];
+            }
+            a = (float)(x.Count * SumXY - SumX * SumY) / (float)(x.Count * SumX2 - SumX * SumX);
+            b = (float)(SumY - a * SumX) / (float)x.Count;
+        }
+
+    }
+}
